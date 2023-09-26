3a68277b7cdf6f6c56a9159f2a3cb76c94280a04
AlinaKalarash <140280974+AlinaKalarash@users.noreply.github.com>
2023-09-26 23:49:33 +0300

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,28 +1,29 @@
-import java.util.LinkedList;
-
 public class Queue {
 
-        private LinkedList<Object> elements = new LinkedList<>();
+    private LinkedList<Object> elements = new LinkedList<>();
 
-        public void add(Object value) {
-            elements.addLast(value);
-        }
-
-        public void clear() {
-            elements.clear();
-        }
-
-        public int size() {
-            return elements.size();
-        }
-
-        public Object peek() {
-            return elements.getFirst();
-        }
-
-        public Object poll() {
-            return elements.pollFirst();
-        }
+    public void add(Object value) {
+        elements.add(value);
     }
 
+    public void clear() {
+        elements.clear();
+    }
 
+    public int size() {
+        return elements.size();
+    }
+
+    public Object peek() {
+        return elements.get(elements.size());
+    }
+
+    public Object poll() {
+        if (elements.size() > 0) {
+            elements.remove(0);
+        } else {
+            throw new RuntimeException("There is no elements left");
+        }
+        return elements.get(0);
+    }
+}
