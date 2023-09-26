044ba7ea4a7a10a24bec95bf35dd28b5cc1d9206
AlinaKalarash <140280974+AlinaKalarash@users.noreply.github.com>
2023-09-26 14:20:28 +0300

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -1,9 +1,10 @@
-public class Entry {
-    Object key;
-    Object value;
+public class Entry<K,V> {
 
-    public Entry(Object key, Object value) {
-        this.key = key;
-        this.value = value;
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
-}
